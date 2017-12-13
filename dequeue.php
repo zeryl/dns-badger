c30a850e9e594c612c14ddfec6c49094b207490e
Chris Partridge <root@co3xist.net>
2017-12-13 04:24:45 -0500

    Fix json_decode in PHP for normal use

--- dequeue.php
+++ dequeue.php
@@ -19,7 +19,7 @@ while(true) {
 	$FQDN = $q->dequeue();
 	if($FQDN !== null) {
 		$res = [];
-		$parsedDomain = $ext->parse(json_decode($FQDN));
+		$parsedDomain = $ext->parse(json_decode($FQDN, true));
 		
 		$gdnsResA = gdnsLooper($parsedDomain->getFullHost(), "A");
 		if($gdnsResA[0]) {
